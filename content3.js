70ab1001a9270c985268f34070da3be68729fa72
nekionodaj <59648608+nekionodaj@users.noreply.github.com>
2020-02-20 19:23:28 +0100

    Delete content3.js

--- content3.js
+++
@@ -1,7 +0,0 @@
-const bg = chrome.extension.getBackgroundPage();
-
-var myElement = document.getElementById("priceblock_ourprice").innerHTML;
-
-if (bg.data.hasOwnProperty(document.url)) {
-      
-}
