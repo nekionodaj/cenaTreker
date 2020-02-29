55c6a25fc50fe95b538dba42158df33dae3aa7ac
mcrz3k <59690137+mcrz3k@users.noreply.github.com>
2020-02-29 11:45:02 +0100

    Add files via upload

--- pricepage.js
+++ pricepage.js
@@ -1,19 +1,12 @@
-function printPrice(tab) {  
+function printPrice(tab) {
   var bg = chrome.extension.getBackgroundPage();
   bg.getPrices();
   Object.keys(bg.data).forEach(function (url) {
-   //  const div = document.createElement('div');
-    // //div.textContent = url + " : " + bg.data[url];
-   //  div.textContent = url + " : ";
-  //   div.className += "alert alert-primary";
-  //   console.log("printam");
- //    console.log(url);
- //    document.body.appendChild(div);
- var h = document.getElementById("popis");
- h.insertAdjacentHTML("afterbegin", `
- <div class="input-group artikli">
-  <div class="input-group-append">
-       <span id="valuta" class="input-group-text">$</span>
+    var h = document.getElementById("popis");
+    h.insertAdjacentHTML("afterbegin", `
+    <div class="input-group artikli">
+   <div class="input-group-append">
+       <span id="valuta" class="input-group-text"></span>
        <span id="cijena" class="input-group-text"></span>
      </div>
    <span id="artikl" type="text" class="form-control"></span>
@@ -22,10 +15,39 @@ function printPrice(tab) {
        <button class="btn btn-outline-danger" type="button">Remove</button>
      </div></div>`);
 
- 
-     document.getElementById('artikl').textContent = url;
-      document.getElementById('cijena').textContent = bg.data[url];  
+    document.getElementById('valuta').textContent = Currency(bg.data[url]);
+    document.getElementById('cijena').textContent = bg.data[url];
+    document.getElementById('artikl').textContent = URLtoName(url, bg.data[url]);
   })
 }
 document.getElementById('printPrice').addEventListener('click', printPrice);
 
+function URLtoName(url, bgCijena) {
+  if (bgCijena.charAt(0) == "\u0024") {
+    //dolar
+    var name = url.substr(19);
+    var first = name.search("/");
+    var last = name.search("/dp/");
+    var formattedName = name.slice(first + 1, last);
+    formattedName = formattedName.replace(/-/g, " ");
+  } else if (bgCijena.charAt(0) == "\u00A3") {
+    //funta
+    var formattedName = url.substr(url.search("/dp/"));
+    formattedName = formattedName.concat(" fali ime .co.uk");
+  } else if (bgCijena.slice(-1) == "\u20ac") {
+    //juro
+    var formattedName = url.substr(url.search("/dp/"));
+    formattedName = formattedName.concat(" fali ime .de");
+  }
+  return formattedName;
+}
+
+function Currency(bgCijena) {
+  if (bgCijena.slice(-1) == "\u20ac") {
+    //juro 
+    var currency = bgCijena.slice(-1);
+  } else
+    //dolar i funta
+    var currency = bgCijena.charAt(0);
+  return currency;
+}
