226610b875c3c0e466b7a8daee4638f596f55e94
nekionodaj <59648608+nekionodaj@users.noreply.github.com>
2020-03-05 17:43:46 +0100

    Add files via upload

--- CenaTreker/pricepage.js
+++ CenaTreker/pricepage.js
@@ -1,26 +1,29 @@
-
-var bg = chrome.extension.getBackgroundPage();
+var bg; // stvori varijablu u koju cemo pohraniti data iz backgrounda
+chrome.runtime.getBackgroundPage(function(bekgraund){bekgraund.getPrices(); bg = bekgraund.data}); //prvo getamo prices u backgroundu i onda ih pohranjujemo u bg
 document.getElementById('printPrice').addEventListener('click', printPrice);
 
 function printPrice(tab) {
-  bg.getPrices();
-  Object.keys(bg.data).forEach(function (url) {
+  //bg.getPrices();
+  Object.keys(bg).forEach(function (url) {
+    if (document.getElementById(url)) document.getElementById(url).remove();
+  });
+  Object.keys(bg).forEach(function (url) { //za svaki kljuc u bg(u biti je to window.data) vrsimo istu funkciju koju smo prije za bg.data
     var h = document.getElementById("popis");
     h.insertAdjacentHTML("afterbegin", `
-    <div class="input-group artikli">
-   <div class="input-group-append">
-       <span id="valuta" class="input-group-text"></span>
-       <span id="cijena" class="input-group-text"></span>
-     </div>
-   <span id="artikl" type="text" class="form-control"></span>
-     <div class="input-group-append">
-       <a href='#' target="_blank" id="gumbLink"><button class="btn btn-outline-warning" type="button">Visit</button></a>
-       <button id="makni" class="btn btn-outline-danger" type="button">Remove</button>
-     </div></div>`);
+    <div id=` + url +  ` class="input-group artikli">
+    <div class="input-group-append">
+    <span id="valuta" class="input-group-text" style="background-color : #DBDBDB"></span>
+    <span id="cijena" class="input-group-text" style="background-color : #DBDBDB"></span>
+    </div>
+    <span id="artikl" type="text" class="form-control"></span>
+    <div class="input-group-append">
+    <a href='#' target="_blank" id="gumbLink"><button id='zutiGumb' class="btn btn-info" data-toggle="tooltip" data-placement="right" title="press to visit item on Amazon">Visit</button></a>
+    <button id="makni" class="btn btn-outline-danger" data-toggle="tooltip" data-placement="right" title="press to remove the item from the list">Remove</button>
+    </div></div>`);
     document.getElementById('gumbLink').href = url;
-    document.getElementById('valuta').textContent = Currency(bg.data[url][0]);
-    document.getElementById('cijena').textContent = bg.data[url][0];
-    document.getElementById('artikl').textContent = bg.data[url][1];
+    document.getElementById('valuta').textContent = Currency(bg[url][0]);
+    document.getElementById('cijena').textContent = makniCurrency(bg[url][0]);
+    document.getElementById('artikl').textContent = bg[url][1];
     document.getElementById('makni').onclick = makni.bind(null, url);
   })
 }
@@ -29,6 +32,7 @@ function makni(url) {
   console.log("makni");
   var link = url
   console.log(link)
+  document.getElementById(url).remove();
   chrome.runtime.sendMessage({data : link, kljuc : "brisanje"}, function(response){console.log("obrisano")}); //salje poruku backgroundu
 }
 
@@ -37,7 +41,17 @@ function Currency(bgCijena) {
     //juro
     var currency = bgCijena.slice(-1);
   } else
-    //dolar i funta
-    var currency = bgCijena.charAt(0);
+  //dolar i funta
+  var currency = bgCijena.charAt(0);
+  return currency;
+}
+
+function makniCurrency(bgCijena) {
+  if (bgCijena.slice(-1) == "\u20ac") {
+    //juro
+    var currency = bgCijena.substring(0, bgCijena.length - 1);
+  } else
+  //dolar i funta
+  var currency = bgCijena.substring(1);
   return currency;
 }
